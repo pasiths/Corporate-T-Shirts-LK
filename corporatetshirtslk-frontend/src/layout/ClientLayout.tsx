d262a2c6da624731d701dfca4cab7579e4f46713
Pasith Senevirathna <pasith.info@gmail.com>
2025-06-11 23:11:51 +0530

    feat: add Footer component and update ClientLayout

--- corporatetshirtslk-frontend/src/layout/ClientLayout.tsx
+++ corporatetshirtslk-frontend/src/layout/ClientLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const ClientLayout = () => {
     return (<div className="min-h-screen">
         <main className="">
             <NavBar/>
             <Outlet />
+            <Footer />
         </main>
     </div>);
 }
