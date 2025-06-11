8175ce14d3bd461e3a2898474109e49a302bf3c5
Pasith Senevirathna <pasith.info@gmail.com>
2025-06-11 23:49:55 +0530

    fix: correct typos and improve layout in Footer component

--- corporatetshirtslk-frontend/src/components/Footer.tsx
+++ corporatetshirtslk-frontend/src/components/Footer.tsx
@@ -3,8 +3,8 @@ import logoIcon from '@/assets/icons/logo.svg';
 const Footer = () => {
     return (
         <footer className="mt-[3.125rem] pb-[1.75rem] font-poppins">
-            <div className="flex justify-between items-start mx-80 mb-9.5">
-                <div className="">
+            <div className="flex justify-between items-start mx-80 mb-[2.375rem]">
+                <div>
                     <h3 className="text-sm font-semibold mb-2.5">Know Us</h3>
                     <p className="text-[12px] text-zinc-700">About Us</p>
                     <p className="text-[12px] text-zinc-700">Contact Us</p>
@@ -12,16 +12,16 @@ const Footer = () => {
                     <p className="text-[12px] text-zinc-700">Careers</p>
                     <p className="text-[12px] text-zinc-700">Elexo Earn</p>
                 </div>
-                <div className="">
+                <div>
                     <h3 className="text-sm font-semibold mb-2.5 ">Policy</h3>
                     <p className="text-[12px] text-zinc-700">Security</p>
                     <p className="text-[12px] text-zinc-700">Privacy</p>
-                    <p className="text-[12px] text-zinc-700">Term of use</p>
+                    <p className="text-[12px] text-zinc-700">Terms of use</p>
                     <p className="text-[12px] text-zinc-700">Return policy</p>
                     <p className="text-[12px] text-zinc-700">Compliance</p>
 
                 </div>
-                <div className="">
+                <div>
                     <h3 className="text-sm font-semibold mb-2.5 ">Help you</h3>
                     <p className="text-[12px] text-zinc-700">Return and Cancelation</p>
                     <p className="text-[12px] text-zinc-700">Membership</p>
@@ -30,7 +30,7 @@ const Footer = () => {
                     <p className="text-[12px] text-zinc-700">Shipping</p>
 
                 </div>
-                <div className="">
+                <div>
                     <h3 className="text-sm font-semibold mb-2.5 ">Social</h3>
                     <p className="text-[12px] text-zinc-700">Facebook</p>
                     <p className="text-[12px] text-zinc-700">Instagram</p>
@@ -40,11 +40,9 @@ const Footer = () => {
 
                 </div>
             </div>
-            <div className="border border-[#424242] mx-5.5">
-                {/* Line */}
-            </div>
+            <hr className="border-t border-[#424242] mx-[1.375rem]" />
             <div className="pt-7.5 mx-40 flex justify-between items-start" >
-                <div className="">
+                <div>
                     <h3 className="text-sm font-semibold mb-2.5">Mail Us</h3>
                     <p className="text-[12px]">
                         <a href="mailto:info@corporatetshirtslk.com" className="text-zinc-600 hover:underline hover:text-zinc-800 ">
@@ -52,10 +50,10 @@ const Footer = () => {
                         </a>
                     </p>
                 </div>
-                <div className="">
+                <div>
                     <img src={logoIcon} alt="logo" className="inline-block md:w-13 md:h-13 w-10 h-10" />
                 </div>
-                <div className="">
+                <div>
                     <h3 className="text-sm font-semibold mb-2.5">Contact us</h3>
                     <p className="text-[12px] text-zinc-700">
                         Corporate T Shirts <br />
