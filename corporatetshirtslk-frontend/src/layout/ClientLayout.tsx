85c016b0500502503db09a10bde674c8a70dc6b9
Pasith Senevirathna <pasith.info@gmail.com>
2025-06-12 15:41:19 +0530

    feat: add background img

--- corporatetshirtslk-frontend/src/layout/ClientLayout.tsx
+++ corporatetshirtslk-frontend/src/layout/ClientLayout.tsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
+import bgImg from '@/assets/img/bg-img.svg';
+
 const ClientLayout = () => {
-    return (<div className="min-h-screen">
-        <NavBar />
-        <main className="">
-            <Outlet />
-        </main>
-        <Footer />
-    </div>);
+    return (
+        <div className="min-h-screen bg-cover bg-center bg-no-repeat"
+    style={{ backgroundImage: `url(${bgImg})` }}>
+            <NavBar />
+            <main className="min-h-[calc(100vh-30rem)]">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default ClientLayout;
